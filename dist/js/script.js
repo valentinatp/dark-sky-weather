0d035f067d5cd9ba1a034cc7fb416fdca951b1d7
Valentina Toledo <valentina.toledoporma@gmail.com>
2017-08-02 02:52:07 -0400

    index 2 ok

--- dist/js/script.js
+++ dist/js/script.js
@@ -12631,6 +12631,13 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 $(document).ready(function() {
+  
+  $("#btn").click(function(e){
+      e.preventDefault();
+      window.open("temperatura.html", "_self", false)
+  });
+
+
   $.ajax({
     url: 'https://api.darksky.net/forecast/8fe9d2d70d0d13ff0c4d66bd56b0a9ef/37.8267,-122.4233',
     type: 'GET',
@@ -12638,19 +12645,26 @@ $(document).ready(function() {
   })
   .done(function(data) {
     console.log(data);
+      
+      function calculoCelsius(a,b){
+        var calculo = parseInt(data.currently.temperature) - 32 / (1.8);
+        return (Math.round(calculo) + '°');
+      }
+
         $('#imagen').append(data.currently.icon);
-        $('#grado').append(data.currently.temperature);
+        $('#grado').append(calculoCelsius);
         $('#wind').append(data.currently.windSpeed + 'm/s');
         $('#humidity').append(data.currently.apparentTemperature + '%');
         $("#uv").append(data.currently.uvIndex);
         $("#pressure").append(data.currently.pressure + ' hPa');
 
-
   })
+
   .fail(function() {
     console.log("error");
   })
-  
-  
+
+
+
 });
 
